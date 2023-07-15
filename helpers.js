a2ee1cf6e14dfc0eb416022cb48de12d7f1263fe
leonardoriossandoval <72031601+leonardoriossandoval@users.noreply.github.com>
2023-07-15 02:22:12 +0200

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,4 @@
+const getDataColors = opacity => {
+    const colors = ['#7448c2', '#21c0d7', '#d99e2b', '#cd3a81', '#9c99cc', '#e14eca', '#ffffff', '#ff0000', '#d6ff00', '#0038ff']
+    return colors.map(color => opacity ? `${color + opacity}` : color)
+}
